5f76b2ace6299ac7e74752cba7efd8bc0fe09be0
Hyojin Jung <hyojinjenny@gmail.com>
2016-05-17 10:56:45 +1200

    Finished to set localstorage for the form

--- js/shipping-form.js
+++ js/shipping-form.js
@@ -23,6 +23,24 @@ if( localStorage.getItem('country') ) {
 
 }
 
+if( localStorage.getItem('tac') == 'true' ) {
+
+	document.querySelector('#tac').checked= true;
+}
+
+if( localStorage.getItem('shipping') ) {
+
+	// Get all the radio buttons
+	var shippingOptions = document.querySelectorAll('[name=shipping]');
+
+	for( var i=0; i<shippingOptions.length; i++) {
+
+		if(shippingOptions[i].id == localStorage.getItem('shipping') ) {
+			shippingOptions[i].setAttribute('checked', 'checked');
+		}
+	}
+}
+
 // Listen for input on the name field
 document.querySelector('#full-name').onkeyup = function(){
 
@@ -35,4 +53,53 @@ document.querySelector('#country').onchange = function(){
 	localStorage.setItem('country', this.value);
 
 }
+
+document.querySelector('#tac').onclick = function(){
+
+	localStorage.setItem('tac', this.checked);
+}
+
+// Listen for clicks for the shipping radio buttons
+// document.querySelector('#shipping-int').onclick = function(){
+
+// 	localStorage.setItem('shipping', this.id);
+// }
+
+// document.querySelector('#shipping-ni').onclick = function(){
+
+// 	localStorage.setItem('shipping', this.id);
+// }
+
+// document.querySelector('#shipping-si').onclick = function(){
+
+// 	localStorage.setItem('shipping', this.id);
+// }
+
+var allShippingOptions = document.querySelectorAll('[name=shipping]');
+
+for(var i=0; i<allShippingOptions.length; i++) {
+
+	allShippingOptions[i].onclick = shipping;
+}
+
+function shipping() {
+	localStorage.setItem('shipping', this.id);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
